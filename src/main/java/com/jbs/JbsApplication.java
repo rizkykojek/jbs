5191921ae7aafd437b6116ed2a0a15a2c4eb5445
rizkykojek <rizki.maulana@happyfresh.com>
2017-02-18 10:14:03 +0700

    "Initial Project"

---
+++ src/main/java/com/jbs/JbsApplication.java
@@ -0,0 +1,12 @@
+package com.jbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbsApplication.class, args);
+	}
+}
