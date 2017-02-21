4d2fb1268f0d920aa92aacc5d1b0a502a71001c6
rizkykojek <rizki.maulana@happyfresh.com>
2017-02-21 20:39:08 +0700

    Fixed error while deploying to tomcat application server, exclude duplicate maven ibrary

--- src/main/java/com/jbs/JbsApplication.java
+++ src/main/java/com/jbs/JbsApplication.java
@@ -2,11 +2,19 @@ package com.jbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JbsApplication {
+public class JbsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JbsApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JbsApplication.class);
+	}
+
 }
