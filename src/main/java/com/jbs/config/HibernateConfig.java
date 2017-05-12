d44b7cbac2e1c31daba17b5c65dfa923f9bb704e
rizkykojek <rizki.maulana@happyfresh.com>
2017-05-12 22:16:52 +0700

    change database schema name

--- src/main/java/com/jbs/config/HibernateConfig.java
+++ src/main/java/com/jbs/config/HibernateConfig.java
@@ -77,7 +77,7 @@ public class HibernateConfig {
 		/*properties.put("hibernate.dialect","org.hibernate.dialect.PostgreSQLDialect");*/
 		properties.put("hibernate.dialect","org.hibernate.dialect.HANARowStoreDialect");
 		//properties.put("hibernate.default_schema","JBSUSER");
-		properties.put("hibernate.default_schema","ycc");
+		properties.put("hibernate.default_schema","S0017067884");
 		properties.put("hibernate.hbm2ddl.auto","create");
 		properties.put("org.hibernate.envers.audit_table_suffix","_audit");
 		return properties;
